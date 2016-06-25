c8f25f8c890c5b353f26f38e8e92b3b0bf11a3bd
Michael Swartz <mjswartz@gmail.com>
2016-06-25 12:14:25 -0400

    models, fixture

--- emails/models.py
+++ emails/models.py
@@ -1,5 +1,22 @@
 from __future__ import unicode_literals
-
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class City(models.Model):
+    display_name = models.CharField(max_length=200, null=False, unique=True)
+    name = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=2, null=False, unique=True)
+
+    def __str__(self):
+        return self.display_name
+
+
+@python_2_unicode_compatible
+class Subscriber(models.Model):
+    email = models.EmailField(null=False, unique=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.email
